a7ca765c19f6a65aca888c8cdde6969d22839e2d
Qualace-Staffing-Solutions-LLP <qualace.staffingsolutions@gmail.com>
2025-04-16 13:32:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,17 +23,17 @@ app.use("/api/clients", clientRoutes);
 
 
 
-// MongoDB Connection
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'MONGO_URI=mongodb+srv://QualAce:Qualace%402024@cluster0.teaz73k.mongodb.net/QualAce_RecruitPro?retryWrites=true';
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    console.log('✅ Connected to MongoDB');
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
+
 
 
   
